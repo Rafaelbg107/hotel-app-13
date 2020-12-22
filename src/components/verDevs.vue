5880291ba08d3b67d28ba7cc987d77b223f35b79
Rafael R. Bustos G <rafaelbg107@gmail.com>
2020-12-21 21:35:12 -0500

    Devs update

---
+++ src/components/verDevs.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div id="Ver">
+            <p>Laura Natalia Castro Garcia  <span>laura.castrogarcia57@gmail.com</span><br>
+            Rafael Rodrigo Bustos González  <span>rafaelbg107@gmail.com</span><br>
+            Franklin De Jesús Martínez Martínez <span>fdmartinez96@gmail.com</span><br>
+            Elias Camilo Cabarcas Carreño   <span>cmilocarreno09@gmail.com</span></p><br>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    #Ver{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-family: "Bahnschrift Light";
+        background-color: #f3f3f3d8
+    }
+    #Ver h1{
+        font-size: 25px;
+        color: #0d1620;
+        font-family: "Bahnschrift Light", "Bahnschrift Light";
+    }
+    #Ver p{
+        font-size: 22px;
+        color: #283747;
+        text-align: justify;
+        
+    }
+    #Ver h3{
+        font-size: 7px;
+        color: #354a61;
+    }
+
+    #Ver span{
+        color: #2592ad;
+        font-weight: normal ;
+        font-family: "Bahnschrift Light","Bahnschrift Light" ;
+        
+    }
+</style>
